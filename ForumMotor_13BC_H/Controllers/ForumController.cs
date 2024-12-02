67379a64a6ea0a60adf8c54a323f45be7901deaf
racz.csaba <racz.csaba.janos@mechwart.com>
2024-12-02 14:34:48 +0100

    Api alap

---
+++ ForumMotor_13BC_H/Controllers/ForumController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ForumMotor_13BC_H.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class ForumController : ControllerBase
+    {
+        
+
+
+    }
+}
